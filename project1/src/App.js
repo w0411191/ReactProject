3253d7c302cf63444fdab464f14aa0d8f867b1cc
dongmyoung kim <w0411191@nscc.ca>
2019-04-24 22:38:55 -0300

    Venue NFO component

--- project1/src/App.js
+++ project1/src/App.js
@@ -3,12 +3,14 @@ import './resources/styles.css';
 
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
+import VenueNfo from './components/venueNfo';
 
 function App() {
   return (
     <div className="App" style={{ height: '1500px', background: 'cornflowerblue' }}>
-      <Header />
-      <Featured />
+      <Header/>
+      <Featured/>
+      <VenueNfo/>
     </div>
   );
 }
