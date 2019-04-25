871d519917ee559ee939962df01c7c7d81ca39b4
dongmyoung kim <w0411191@nscc.ca>
2019-04-24 23:04:48 -0300

    Highlight section

--- project1/src/App.js
+++ project1/src/App.js
@@ -4,13 +4,15 @@ import './resources/styles.css';
 import Header from './components/header_footer/Header';
 import Featured from './components/featured';
 import VenueNfo from './components/venueNfo';
+import Highlights from './components/Highlights';
 
 function App() {
   return (
-    <div className="App" style={{ height: '1500px', background: 'cornflowerblue' }}>
+    <div className="App" style={{ height: '1000px', background: 'cornflowerblue' }}>
       <Header/>
       <Featured/>
       <VenueNfo/>
+      <Highlights/>
     </div>
   );
 }
---
+++ project1/src/components/Highlights/Description.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Fade from 'react-reveal/Fade';
+
+const Description = () => {
+    return (
+        <Fade>
+            <div className="center_wrapper">
+                <h2>Hightlights</h2>
+                <div className="highlight_description">
+                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.
+                </div>
+            </div>
+        </Fade>
+    );
+};
+
+export default Description;
