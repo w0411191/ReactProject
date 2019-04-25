c3a90a3e850d87dfcac065a718cc64661be2ac2e
dongmyoung kim <w0411191@nscc.ca>
2019-04-24 23:16:31 -0300

    Button reusable component

--- project1/src/components/Highlights/Discount.js
+++ project1/src/components/Highlights/Discount.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Fade from 'react-reveal/Fade';
 import Slide from 'react-reveal/Slide';
 
+import MyButton from '../utils/MyButton';
+
 class Discount extends Component {
 
     state = {
@@ -42,9 +44,12 @@ class Discount extends Component {
                             <h3>Purchase tickets before 20th JUNE</h3>
                             <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
                             
-                            <div>
-                                button
-                            </div>
+                            <MyButton
+                                text="Purchase tickets"
+                                bck="#ffa800"
+                                color="#ffffff"
+                                link="http://google.com"
+                            />
                         </div>
                     </Slide>
 
