34325d760707294664135bc916766a774ea87c87
dongmyoung kim <w0411191@nscc.ca>
2019-04-25 10:08:03 -0300

    Home:featured Animation

--- m_city/src/Components/Header_footer/Header.js
+++ m_city/src/Components/Header_footer/Header.js
@@ -15,7 +15,7 @@ class Header extends Component {
                 style={{
                     backgroundColor: '#98c5e9',
                     boxShadow: 'none',
-                    padding: '10px, 0',
+                    padding: '10px 0',
                     borderBottom: '2px solid #00285e'
                 }}
             >
