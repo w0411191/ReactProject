e3a036e827203c638dc38989ac7afa68163247dd
dongmyoung kim <w0411191@nscc.ca>
2019-04-25 22:40:39 -0300

    42. Home: Finishing Enroll and form fields

--- m_city/src/Components/home/promotion/Enroll.js
+++ m_city/src/Components/home/promotion/Enroll.js
@@ -3,6 +3,8 @@ import Fade from 'react-reveal/Fade';
 import FormField from '../../ui/formFields';
 import { validate } from '../../ui/misc';
 
+import { firebasePromotions } from '../../../firebase';
+
 class Enroll extends Component {
 
     state = {
@@ -43,6 +45,31 @@ class Enroll extends Component {
         this.setState({
             formError: false,
             formdata: newFormdata
+        });
+        this.successMessage();
+    }
+
+    successMessage() {
+        setTimeout(() => {
+            this.setState({
+                formSuccess: ''
+            })
+        }, 2000)
+    }
+
+    resetFormSuccess(type) {
+        const newFormdata = {...this.state.formdata}
+
+        for(let key in newFormdata) {
+            newFormdata[key].value = '';
+            newFormdata[key].valid = false;
+            newFormdata[key].validationMessage = '';
+        }
+
+        this.setState({
+            formError: false,
+            formdata: newFormdata,
+            formSuccess: type ? 'Congratulations' : 'Already on the database'
         })
     }
 
@@ -57,7 +84,18 @@ class Enroll extends Component {
             formIsValid = this.state.formdata[key].valid && formIsValid;
         }
         if(formIsValid) {
-            console.log(dataToSubmit)
+            firebasePromotions.orderByChild('email').equalTo(dataToSubmit.email).once("value")
+            .then((snapshot) => {
+                if(snapshot.val() === null) {
+                    firebasePromotions.push(dataToSubmit);
+                    this.resetFormSuccess(true);
+                } else {
+                    this.resetFormSuccess(false);
+                }
+                snapshot.val()
+            })
+
+            //this.resetFormSuccess();
         } else {
             this.setState({
                 formError: true
@@ -86,7 +124,11 @@ class Enroll extends Component {
                                 <div className="error_label">Something is wrong, try again!</div> 
                                 : null 
                             }
+                            <div className="success_label">{ this.state.formSuccess }</div>
                             <button onClick={(event) => this.submitForm(event)}>Enroll</button>
+                            <div className="enroll_discl">
+                            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            </div>
                         </div>
                     </form>
                 </div>
