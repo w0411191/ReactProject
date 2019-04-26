794d52e20010f05ae265e0acf14c5d44998a7ab8
dongmyoung kim <w0411191@nscc.ca>
2019-04-26 12:46:52 -0300

    49. Admin: Edit Match - creating the form

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -7,13 +7,16 @@ import PublicRoute from  './Components/authRoutes/publicRoutes';
 
 import Home from './Components/home';
 import SignIn from './Components/signin';
+
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
+import AddEditMatch from './Components/admin/matches/addEditMatch';
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+        <PrivateRoute {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch}/>
         <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches}/>
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
         <PublicRoute {...props} restricted={true} path="/sign_in" exact component={SignIn}/>
