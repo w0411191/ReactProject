65d584467564107f8f3fbc2d9ad6dd89b6380ff9
dongmyoung kim <w0411191@nscc.ca>
2019-04-26 11:27:48 -0300

    46. Auth: Private Routes

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoute from './Components/authRoutes/privateRoutes';
+
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import Dashboard from './Components/admin/Dashboard';
@@ -10,7 +12,8 @@ const Routes = (props) => {
   return (
     <Layout>
       <Switch>
-        <Route exact component={Dashboard} path="/dashboard"/>
+
+        <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
         <Route exact component={SignIn} path="/sign_in"/>
         <Route exact component={Home} path="/"/>
       </Switch>
