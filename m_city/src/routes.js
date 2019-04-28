32d79d37bd55e24c3dc883da506fe2b5c302f8b8
dongmyoung kim <w0411191@nscc.ca>
2019-04-28 01:38:13 -0300

    65. Adding a 404 PAGE

--- m_city/src/routes.js
+++ m_city/src/routes.js
@@ -9,6 +9,7 @@ import TheTeam from './Components/theTeam';
 import Home from './Components/home';
 import SignIn from './Components/signin';
 import TheMatches from './Components/theMatches';
+import NotFound from './Components/ui/not_found';
 
 import Dashboard from './Components/admin/Dashboard';
 import AdminMatches from './Components/admin/matches';
@@ -31,6 +32,7 @@ const Routes = (props) => {
         <PublicRoute {...props} restricted={false} path="/the_matches" exact component={TheMatches}/>
         <PublicRoute {...props} restricted={false} path="/the_team" exact component={TheTeam}/>
         <PublicRoute {...props} restricted={false} path="/" exact component={Home}/>
+        <PublicRoute {...props} restricted={false} exact component={NotFound}/>
       </Switch>
     </Layout>
   )
