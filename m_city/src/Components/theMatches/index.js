159709b5a88365bccde58db24e9c37af15dc7ab5
dongmyoung kim <w0411191@nscc.ca>
2019-04-28 01:24:29 -0300

    63. The matches: Filters

--- m_city/src/Components/theMatches/index.js
+++ m_city/src/Components/theMatches/index.js
@@ -29,6 +29,17 @@ class TheMatches extends Component {
         })
     }
 
+    showPlayed = (played) => {
+        const list = this.state.matches.filter((match) => {
+            return match.final === played
+        });
+        this.setState({
+            filterMatches: played === 'All' ? this.state.matches : list,
+            playerFilter: played,
+            resultFilter: 'All'
+        })
+    }
+
     render() {
         const state = this.state;
         return (
@@ -36,8 +47,30 @@ class TheMatches extends Component {
                 <div className="the_matches_wrapper">
                     <div className="left">
                         <div className="match_filters">
-                            <MatchesList matches={state.filterMatches}/>
+                            <div className="match_filters_box">
+                                <div className="tag">
+                                    Show Match
+                                </div>
+                                <div className="cont">
+                                    <div className={`option`}
+                                        onClick={() => this.showPlayed('All')}
+                                    >
+                                        All
+                                    </div>
+                                    <div className={`option`}
+                                        onClick={() => this.showPlayed('Yes')}
+                                    >
+                                        Played
+                                    </div>
+                                    <div className={`option`}
+                                        onClick={() => this.showPlayed('No')}
+                                    >
+                                        Not played
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                        <MatchesList matches={state.filterMatches}/>
                     </div>
                     <div className="right">
                         <LeagueTable/>
